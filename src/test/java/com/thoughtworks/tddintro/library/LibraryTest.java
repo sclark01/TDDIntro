7d26588844c5c36aa58fd69270d435f2253ba007
sclark01 <sclark.tieche@gmail.com>
2015-07-18 23:37:29 +0200

    verified the println is never invoked when there are no books

--- src/test/java/com/thoughtworks/tddintro/library/LibraryTest.java
+++ src/test/java/com/thoughtworks/tddintro/library/LibraryTest.java
@@ -10,9 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Matchers.contains;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class LibraryTest {
 
@@ -23,10 +21,12 @@ public class LibraryTest {
 
      */
     private List<String> books;
+    private PrintStream printStream;
 
     @Before
     public void constructBookList(){
         books = new ArrayList<>();
+        printStream = mock(PrintStream.class);
     }
 
     @Test
@@ -34,7 +34,7 @@ public class LibraryTest {
 
         String title = "Book Title";
         books.add(title);
-        PrintStream printStream = mock(PrintStream.class);
+
         Library library = new Library(books, printStream, null);
 
         library.listBooks();
@@ -44,8 +44,9 @@ public class LibraryTest {
 
     @Test
     public void shouldPrintNothingWhenThereAreNoBooks() {
-
-        // implement me
+        Library library = new Library(books, printStream, null);
+        library.listBooks();
+        verify(printStream, never()).println();
     }
 
     @Test
