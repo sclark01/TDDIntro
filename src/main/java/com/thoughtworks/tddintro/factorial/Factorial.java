e904c55082f26cf8b45d3366e0fbb39446f804fe
sclark01 <sclark.tieche@gmail.com>
2015-07-18 16:08:38 +0200

    Changed if statement from i < 1, to i == 0. Test 1-4 still passing.

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,7 +2,7 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i < 1) {
+        if (i == 0) {
             return 1;
         }
         return i * compute(i - 1);
