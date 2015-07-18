6a7760cd53cff200fd3ea871cd3aca6fa40efce9
sclark01 <sclark.tieche@gmail.com>
2015-07-18 16:10:54 +0200

    Throws IllegalArgumentException when given a number less than zero. All tests passing

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,9 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException("Numbers cannot be negative.");
+        }
         if (i == 0) {
             return 1;
         }
